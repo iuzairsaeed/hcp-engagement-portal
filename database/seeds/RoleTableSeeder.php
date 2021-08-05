83652292679a34a5da25705afc21f3125e51b600
Uzair Saeed <uzair.saeed27@gmail.com>
2021-08-05 13:52:48 +0500

    implement auth, made roles & permissions , basic structure ready

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        
+        // Role Admin
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $permissions = Permission::pluck('id','id')->all();
+        $roleAdmin->syncPermissions($permissions);
+        
+        // Role Normal
+        // $roleNormal = Role::create(['name' => 'normal']);
+        // $normalPermission = [  
+        //     'plan-getList',
+        //     'plan-list',
+        //     'plan-create',
+        //     'plan-edit',
+        //     'plan-delete',
+        // ];
+        $roleNormal = Role::create(['name' => 'admin']);
+        $normalPermission = Permission::pluck('id','id')->all();
+        $permissionsN = Permission::whereIn('name',$normalPermission)->pluck('id');
+        $roleNormal->syncPermissions($permissionsN);
+    }
+}
