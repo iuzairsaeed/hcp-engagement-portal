aef7889fb7e2e927ebb17abb5526dfc88eeb437f
Uzair Saeed <uzair.saeed27@gmail.com>
2021-08-14 02:21:50 +0500

    debug

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => storage_path('app'),
             'visibility' => 'public',
         ],
 
