0c74be5735e86d7ea739fc8e2fa867a232cd40e3
Uzair Saeed <uzair.saeed27@gmail.com>
2021-08-14 06:22:06 +0500

    dd view

--- app/Http/Controllers/Web/PostController.php
+++ app/Http/Controllers/Web/PostController.php
@@ -74,8 +74,12 @@ class PostController extends Controller
     {
         try {
             $data = $request->all();
+            $data['user_id'] = auth()->id();
             if($request->hasFile('post_image')){ 
-                $file_name = uploadFile($request->post_image, postPath());
+                $a = $request->file('post_image')->store(
+                    'post_images', 'public'
+                );
+                // $file_name = uploadFile($request->post_image, postPath());
                 $data['post_image'] = $file_name;
             }
             $this->model->create($data);
